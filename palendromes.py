c451deb66779a36d2ed561cd1c23228d1b36353d
Derek <derekschwenke@gmail.com>
2017-02-26 19:50:00 -0500

    Update spelling dwarves.

--- palendromes.py
+++ palendromes.py
@@ -10,14 +10,15 @@ def is_palindrome(n):
     return n == n[::-1]
 
 
-Dwarfs = ["Gimli", "Fili", "Ilif", "Ilmig", "Mark"]
-dwarfs = [clean_name(d) for d in Dwarfs]
+Dwarves = ["Gimli", "Fili", "Ilif", "Ilmig", "Mark"]
+dwarves = [clean_name(d) for d in Dwarves]
 
-for d in dwarfs:
-    if dwarfs.count(d) != 1:
+for d in dwarves:
+    if dwarves.count(d) != 1:
         raise Exception('Duplicate name not supported: '+d)
 
-for i in range(1, len(dwarfs)+1):
-    for sequence in permutations(dwarfs, i):
+for i in range(1, len(dwarves)+1):
+    for sequence in permutations(dwarves, i):
         if is_palindrome("".join(sequence)):
-            print " ".join([Dwarfs[dwarfs.index(d)] for d in sequence])
+            print " ".join([Dwarves[dwarves.index(d)] for d in sequence])
+
