64bd4ddef6774ce5f86f88275fcb8d8bc6bdba2f
Derek <derekschwenke@gmail.com>
2017-09-27 12:13:00 -0400

    Update palendromes.py

--- palendromes.py
+++ palendromes.py
@@ -11,14 +11,14 @@ def is_palindrome(n):
 
 
 Dwarves = ["Gimli", "Fili", "Ilif", "Ilmig", "Mark"]
-dwarves = [clean_name(d) for d in Dwarves]
+dwarves = [clean_name(d) for d in Dwarves]  # clean input
 
-for d in dwarves:
+for d in dwarves:  # check input
     if dwarves.count(d) != 1:
         raise Exception('Duplicate name not supported: '+d)
 
-for i in range(1, len(dwarves)+1):
-    for sequence in permutations(dwarves, i):
+for i in range(1, len(dwarves)+1):  # all lengths
+    for sequence in permutations(dwarves, i):  # all orders
         if is_palindrome("".join(sequence)):
             print " ".join([Dwarves[dwarves.index(d)] for d in sequence])
 
